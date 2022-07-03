b0f8477e6d61ea0683a4bea45a5b4944ab25034e
MrGeorgeK55 <georgek55_55@hotmail.com>
2022-07-02 19:40:51 -0600

    spring setup

---
+++ src/main/java/com/George/GKI/GkiApplication.java
@@ -0,0 +1,13 @@
+package com.George.GKI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/George/GKI/GkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.George.GKI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
